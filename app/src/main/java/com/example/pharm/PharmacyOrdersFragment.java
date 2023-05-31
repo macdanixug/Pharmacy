52310bf80a56edf20c7ee0961108cbd75d23c184
macdanixug <relnusdan1@gmail.com>
2023-05-31 19:34:50 +0300

    Cart

--- app/src/main/java/com/example/pharm/PharmacyOrdersFragment.java
+++ app/src/main/java/com/example/pharm/PharmacyOrdersFragment.java
@@ -3,14 +3,21 @@ package com.example.pharm;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class PharmacyOrdersFragment extends Fragment {
-
+    private RecyclerView recyclerView;
+    private viewOrderAdapter adapter;
+    private List<OrderItem> orderItems;
 
     public PharmacyOrdersFragment() {
         // Required empty public constructor
@@ -20,7 +27,15 @@ public class PharmacyOrdersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pharmacy_orders, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_pharmacy_orders, container, false);
+
+        recyclerView = view.findViewById(R.id.products);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        orderItems = new ArrayList<>();
+        adapter = new viewOrderAdapter(orderItems);
+        recyclerView.setAdapter(adapter);
+
+        return view;
+
+        }
 }
---
+++ app/src/main/java/com/example/pharm/viewOrderAdapter.java
@@ -0,0 +1,53 @@
+package com.example.pharm;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class viewOrderAdapter extends RecyclerView.Adapter<viewOrderAdapter.ViewHolder> {
+    private List<OrderItem> orderItems;
+
+    public viewOrderAdapter(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView productNameTextView;
+        TextView productQuantityTextView;
+        TextView productPriceTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            productNameTextView = itemView.findViewById(R.id.productNameTextView);
+            productQuantityTextView = itemView.findViewById(R.id.productQuantityTextView);
+            productPriceTextView = itemView.findViewById(R.id.productPriceTextView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.view_order_design, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        OrderItem orderItem = orderItems.get(position);
+        holder.productNameTextView.setText(orderItem.getProductName());
+        holder.productQuantityTextView.setText(String.valueOf(orderItem.getProductQuantity()));
+        holder.productPriceTextView.setText(String.valueOf(orderItem.getProductPrice()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return orderItems.size();
+    }
+}
