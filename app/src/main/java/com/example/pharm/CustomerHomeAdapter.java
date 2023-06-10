bfde1cd10ea4107ec1723c30b0366c8adbac2bd7
macdanixug <relnusdan1@gmail.com>
2023-06-10 10:37:58 +0300

    Customer Home Fragment edit

--- app/src/main/java/com/example/pharm/CustomerHomeAdapter.java
+++ app/src/main/java/com/example/pharm/CustomerHomeAdapter.java
@@ -35,7 +35,7 @@ public class CustomerHomeAdapter extends RecyclerView.Adapter<CustomerHomeAdapte
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.name.setText(drugs.get(position).getName());
-        holder.description.setText(drugs.get(position).getDescription());
+        holder.description.setText("UGX Shs: "+drugs.get(position).getPrice());
 
         Picasso.get().load(drugs.get(position).getImageUrl()).into(holder.drug_image);
 
